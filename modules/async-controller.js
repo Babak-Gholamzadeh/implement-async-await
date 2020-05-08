26b540a09aa11ab6da3c88d5f7bca065ad3928c2
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2020-05-08 08:39:03 +0430

    use an iterator function to repeat its operations

--- modules/async-controller.js
+++ modules/async-controller.js
@@ -1,8 +1,16 @@
 function asyncController(genFunction) {
   var genResult = genFunction();
-  var yieldResult = genResult.next();
-  var promiseObject = yieldResult.value;
-  promiseObject.then(function(data) {
-    genResult.next(data);
-  });
+
+  iterator();
+  function iterator(arg) {
+
+    var yieldResult = genResult.next(arg);
+    var promiseObject = yieldResult.value;
+    promiseObject.then(function (data) {
+      // genResult.next(data);
+      iterator(data);
+    });
+
+  }
+
 }
